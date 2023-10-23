7744cec72a630e01174f118884873190d91d4eb5
John Bruckler <john.bruckler@dtmidstream.com>
2023-10-23 17:01:31 -0400

    [skip ci] added singleton for maintaining config file path

--- src/Cmdlets/ImportPSSconfig.cs
+++ src/Cmdlets/ImportPSSconfig.cs
@@ -1,3 +1,4 @@
+
 using System.IO;
 using System.Management.Automation;
 using Newtonsoft.Json;
@@ -6,15 +7,50 @@ using PSSimpleConfig.Utilities;
 
 namespace PSSimpleConfig.Cmdlets;
 
-[Cmdlet(VerbsData.Import, "PSSConfig")]
-public class ImportPSSConfig :PSCmdlet
+[Cmdlet(VerbsData.Import, "PSSConfig", DefaultParameterSetName = "Module")]
+public class ImportPSSConfig : PSCmdlet
 {
-    [Parameter(Mandatory = false)]
-    public DirectoryInfo PathOverride { get; set; } = new DirectoryInfo(Directory.GetCurrentDirectory());
+    [Parameter(Mandatory = true, ParameterSetName = "Module")]
+    public string? ModuleName { get; set; }
+
+    [Parameter(Mandatory = true, ParameterSetName = "File")]
+    public FileInfo ConfigFile { get; set; }
     protected override void ProcessRecord()
     {
-        WriteVerbose($"Initializing configuration from {PathOverride.FullName}");
-        FileInfo _configPath = PSSC.Initialize(PathOverride);
+        PSSC instance = PSSC.Instance; // Initialize the singleton instance
+
+        if (ParameterSetName == "Module" && ModuleName is null)
+        {
+            throw new PSArgumentNullException(nameof(ModuleName));
+        }
+        else if (ParameterSetName == "File" && ConfigFile is null)
+        {
+            throw new PSArgumentNullException(nameof(ConfigFile));
+        }
+
+        if (ModuleName is not null)
+        {
+            PowerShell ps = PowerShell.Create();
+            ps.AddCommand("Get-Module").AddParameter("Name", ModuleName).AddParameter("ListAvailable");
+            var result = ps.Invoke();
+            if (result.Count == 0)
+            {
+                throw new FileNotFoundException($"Unable to find module {ModuleName}");
+            }
+            WriteVerbose($"Initializing configuration for module {ModuleName} with base path {result[0].Properties["ModuleBase"].Value}");
+            instance.Initialize(new DirectoryInfo(result[0].Properties["ModuleBase"].Value.ToString()));
+        }
+        else if (ConfigFile is not null)
+        {
+            WriteVerbose($"Initializing configuration for project {ConfigFile.Name}");
+            instance.Initialize(ConfigFile);
+        }
+        else
+        {
+            WriteVerbose($"Initializing configuration for project using current directory {Path.Combine(Directory.GetCurrentDirectory(), "config", "config.json")}");
+            instance.Initialize(new DirectoryInfo(Directory.GetCurrentDirectory()));
+        }
+        FileInfo _configPath = instance.ConfigPath;
 
         WriteVerbose($"Reading configuration from {_configPath.FullName}");
         WriteObject(JObject.Parse(File.ReadAllText(_configPath.FullName)));
--- src/Utilities/PSSC.cs
+++ src/Utilities/PSSC.cs
@@ -1,61 +1,82 @@
-using System.IO;
-using System.Management.Automation;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace PSSimpleConfig.Utilities;
 
-public static class PSSC
+public class PSSC
 {
-    public static FileInfo Initialize(DirectoryInfo directory)
-    {
-        string _configPath = Path.Combine(directory.FullName, "config", "config.json");
+    private static readonly Lazy<PSSC> _instance = new Lazy<PSSC>(() => new PSSC());
 
-        if (!Directory.Exists(Path.Combine(directory.FullName, "config")))
+    private FileInfo _configPath;
+    public FileInfo ConfigPath => _configPath;
+
+    private PSSC()
+    {
+        // Initialize with some default value if needed
+        _configPath = new FileInfo(Path.Combine(Directory.GetCurrentDirectory(), "config", "config.json"));
+    }
+
+    public static PSSC Instance => _instance.Value;
+
+    public void Initialize(DirectoryInfo directory)
+    {
+        Initialize(new FileInfo(Path.Combine(directory.FullName, "config", "config.json")));
+    }
+
+    public void Initialize(FileInfo fileInfo) {
+        if (fileInfo is null) {
+            throw new ArgumentNullException(nameof(fileInfo));
+        }
+
+        string pathToCfgFile = fileInfo.FullName;
+        DirectoryInfo directory = new DirectoryInfo(fileInfo.DirectoryName);
+
+        if (!Directory.Exists(directory.FullName))
         {
             try {
-                Directory.CreateDirectory(Path.Combine(directory.FullName, "config"));
+                Directory.CreateDirectory(directory.FullName);
             }
             catch (Exception e) {
                 throw new IOException($"Unable to create config directory for project: {directory.Name}. Error: {e.Message}");
             }
         }
 
-        if (!File.Exists(_configPath))
+        if (!File.Exists(pathToCfgFile))
         {
             try {
-                File.WriteAllText(_configPath, "{}");
+                File.WriteAllText(pathToCfgFile, "{}");
             }
             catch (Exception e) {
-                throw new IOException($"Unable to create config file for project: {directory.Name}. Error: {e.Message}");
+                throw new IOException($"Unable to create config file for project: {pathToCfgFile}. Error: {e.Message}");
             }
         }
-        return new FileInfo(_configPath);
+        _configPath = new FileInfo(pathToCfgFile);
     }
 
-    public static JObject ImportConfig(DirectoryInfo directory)
+    public JObject ImportConfig(FileInfo configFilePath)
     {
-        FileInfo _configPath = Initialize(directory);
+        // If the path is null, use the default path
+        configFilePath ??= _configPath;
         try {
-            if (!File.Exists(_configPath.FullName))
-            {
-                throw new FileNotFoundException($"Configuration file not found at path: {_configPath.FullName}");
-            }
-            return JObject.Parse(File.ReadAllText(_configPath.FullName));
+            return !File.Exists(configFilePath.FullName)
+                ? throw new FileNotFoundException($"Configuration file not found at path: {configFilePath.FullName}")
+                : JObject.Parse(File.ReadAllText(configFilePath.FullName));
         }
         catch (Exception e) {
-            throw new InvalidOperationException($"Unable to import configuration file for project: {directory.Name}. Error: {e.Message}");
+            throw new InvalidOperationException($"Unable to import configuration file for project: {configFilePath.Name}. Error: {e.Message}");
         }
     }
 
-    public static void ExportConfig(DirectoryInfo directory, JObject jObject)
+    public void ExportConfig(FileInfo configFilePath, JObject jObject)
     {
-        FileInfo _configPath = Initialize(directory);
+        // If the path is null, use the default path
+        configFilePath ??= _configPath;
+
         try {
-            File.WriteAllText(_configPath.FullName, jObject.ToString());
+            File.WriteAllText(configFilePath.FullName, jObject.ToString());
         }
         catch (Exception e) {
-            throw new InvalidOperationException($"Unable to export configuration file for project: {directory.Name}. Error: {e.Message}");
+            throw new InvalidOperationException($"Unable to export configuration file for project: {configFilePath.Name}. Error: {e.Message}");
         }
     }
 }
